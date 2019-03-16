80cdc2fbd29575bfb7e954c7cb58124d8dbe02fb
bangdasun <bangdasun94@gmail.com>
2019-03-15 22:25:40 -0500

    create metrics module and add keras callbacks

---
+++ metrics/base_metrics.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+
+def rmse(y_true, y_pred):
+    assert len(y_true) == len(y_pred)
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+
+def multilabel_f1(y_true, y_pred):
+    """
+
+    Refer from @author: onodera @address: https://www.kaggle.com/onodera
+
+    Multi-labels f1 score
+
+    :param y_true    : iterable object (list, array)
+    :param y_pred    : iterable object (list, array)
+    :return f1_score : float number
+
+    Examples
+    --------
+    >>> y_true = [1, 2, 3]
+    >>> y_pred = [2, 3]
+    >>> multilabel_f1(y_true, y_pred)
+    ... 0.8
+    """
+    y_true, y_pred = set(y_true), set(y_pred)
+    precision = np.sum([1 for i in y_pred if i in y_true]) / len(y_pred)
+    recall = np.sum([1 for i in y_true if i in y_pred]) / len(y_true)
+    if precision + recall == 0:
+        return 0
+    else:
+        return (2 * precision * recall) / (precision + recall)
---
+++ metrics/keras_callbacks.py
@@ -0,0 +1,59 @@
+
+import numpy as np
+from sklearn.metrics import roc_auc_score, average_precision_score, f1_score
+from keras.callbacks import Callback
+
+
+class RocAucEvaluation(Callback):
+    """ Calculate ROC AUC """
+    def __init__(self, validation_data, interval=1):
+        """
+        :param validation_data : tuple, validation data (X, y)
+        :param interval        : int, number of epochs between checkpoints
+        """
+        super().__init__()
+        self.X_valid, self.y_valid = validation_data
+        self.interval = interval
+    
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch % self.interval == 0:
+            y_pred = self.model.predict(self.X_valid, verbose=0)
+            score = roc_auc_score(self.y_valid, y_pred)
+            print('ROC AUC - epoch: {:d} - score: {:.5f}'.format(epoch + 1, score))
+
+            
+class PRAucEvaluation(Callback):
+    """ Calculate Precision - Recall AUC """
+    def __init__(self, validation_data, interval=1):
+        """
+        :param validation_data : tuple, validation data (X, y)
+        :param interval        : int, number of epochs between checkpoints
+        """
+        super().__init__()
+        self.X_valid, self.y_valid = validation_data
+        self.interval = interval
+    
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch % self.interval == 0:
+            y_pred = self.model.predict(self.X_valid, verbose=0)
+            score = average_precision_score(self.y_valid, y_pred)
+            print('PR AUC - epoch: {:d} - score: {:.5f}'.format(epoch + 1, score))
+
+            
+class F1Evaluation(Callback):
+    """ Calculate F1 score """
+    def __init__(self, validation_data, interval=1):
+        """
+        :param validation_data : tuple, validation data (X, y)
+        :param interval        : int, number of epochs between checkpoints
+        """
+        super().__init__()
+        self.X_valid, self.y_valid = validation_data
+        self.interval = interval
+    
+    def on_epoch_end(self, epoch, threshold=0.5, logs=None):
+        if epoch % self.interval == 0:
+            y_pred_proba = self.model.predict(self.X_valid, verbose=0)
+            y_pred = (y_pred_proba > threshold).astype(np.int)
+            score = f1_score(self.y_valid, y_pred)
+            print('F1 - epoch: {:d} - score: {:.5f} threshold = {:.3f}'.format(epoch + 1, score, threshold))
