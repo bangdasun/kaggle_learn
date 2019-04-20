c0979b8a8e0eb8dd902bad09e16eacbdc708ac0d
bangdasun <bangdasun94@gmail.com>
2019-04-19 20:28:34 -0500

    return optimized dtypes for reduce_memory_usage and reorganize doc strings

--- utils.py
+++ utils.py
@@ -10,16 +10,14 @@ from contextlib import contextmanager
 
 @contextmanager
 def timer(name):
-    """ @author: lopuhin @address https://www.kaggle.com/lopuhin/ 
-    """
+    """ @author: lopuhin @address https://www.kaggle.com/lopuhin/ """
     start_time = time.time()
     yield
     print('[{} done in {:.3f} s.]'.format(name, time.time() - start_time))
     
 
 def memory_usage():
-    """ @author: rdizzl3 @address: https://www.kaggle.com/rdizzl3
-    """
+    """ @author: rdizzl3 @address: https://www.kaggle.com/rdizzl3 """
     pid = os.getpid()
     py = psutil.Process(pid)
     memory_use = py.memory_info()[0] / 2. ** 30
@@ -27,10 +25,10 @@ def memory_usage():
 
 
 def reduce_memory_usage(df):
-    """ @author: gemartin @address: https://www.kaggle.com/gemartin        
-    """
+    """ @author: gemartin @address: https://www.kaggle.com/gemartin """
     start_mem = df.memory_usage().sum() / 1024 ** 2
     print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    dtypes = {}
     
     for col in df.columns:
         col_type = df[col].dtype
@@ -41,39 +39,43 @@ def reduce_memory_usage(df):
             if str(col_type)[:3] == 'int':
                 if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
                     df[col] = df[col].astype(np.int8)
+                    dtypes[col] = np.int8
                 elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
                     df[col] = df[col].astype(np.int16)
+                    dtypes[col] = np.int16
                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
+                    dtypes[col] = np.int32
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)  
+                    df[col] = df[col].astype(np.int64)
+                    dtypes[col] = np.int64
             else:
                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
+                    dtypes[col] = np.float16
                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
+                    dtypes[col] = np.float32
                 else:
                     df[col] = df[col].astype(np.float64)
+                    dtypes[col] = np.float64
         else: 
             df[col] = df[col].astype('category')
+            dtypes[col] = 'category'
 
     end_mem = df.memory_usage().sum() / 1024 ** 2
     print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
     print('Decreased by {:.2f}%'.format(100 * (start_mem - end_mem) / start_mem))
-    return df
-	
+    return df, dtypes
+
 
 def get_logger(logger_name):
-    """Get logger to log information during code running
-	
-    Parameters
-    ----------
-    logger_name: str, title of log file 
-	
-    Returns
-    -------
-    logger     : logger object
-    
+    """
+    Get logger to log information during code running
+
+    :param logger_name: str, title of log file
+    :return logger    : logger object
+
     Examples
     --------
     >>> logger_main = get_logger('main')
