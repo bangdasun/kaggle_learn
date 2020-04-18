b3fd6f352b4fe8cca3947f57dcbbc94f006462b8
bangdasun <bangdasun94@gmail.com>
2020-04-18 16:48:50 -0400

    create utils module in feature_engineering

---
+++ feature_engineering/utils.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+from sklearn.base import TransformerMixin
+
+
+class FeatureSelector(TransformerMixin):
+    """ Select features from pandas dataframe """
+    def __init__(self, features):
+        if isinstance(features, list):
+            self.features = features
+        elif isinstance(features, str):
+            self.features = [features]
+        else:
+            raise ValueError('`feature` cannot be in type: {}'.format(type(features)))
+
+    def fit(self, df: pd.DataFrame, y=None):
+        return self
+
+    def transform(self, df: pd.DataFrame):
+        return df[self.features]
